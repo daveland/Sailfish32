53302489ab88dde1a3cad9ed479f88d170beb5aa
davelandia <davelandia@verizon.net>
2012-05-23 20:53:06 -0400

    added Gccapplication1

---
+++ Marlin/GccApplication1.cpp
@@ -0,0 +1,26 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 5/17/2012 11:43:43 PM
+ *  Author: danderson
+ */ 
+
+
+#include <avr32/io.h>
+
+class foo
+{
+public:
+ int var1;
+protected:
+private:
+};
+
+int main(void)
+{ foo x;
+    while(1)
+    { x.var1=5;
+		
+        //TODO:: Please write your application code 
+    }
+}
